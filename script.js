1ffd74c353b139c19c77bb8ca9ed17c7f75a7fe8
treesmad <nmal@wayne.edu>
2015-11-12 09:22:51 -0500

    basic remake of reddit's r/aww using ajax

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $.get('https://www.reddit.com/r/aww/.json',function(response){
+
+      var posts = response.data.children;
+
+    $.each(posts, function(index, post){
+
+      var $container =  $('<div />').appendTo($('body'));
+      var $title = $('<a />').html("<h1>" + post.data.title + "</h1>").appendTo($container);
+      $title.attr('href', post.data.url);
+      $('<img />').attr('src', post.data.thumbnail).appendTo($container);
+      $('<p />').text('Posted by: ' + post.data.author).appendTo($container);
+
+    var postDate = new Date(post.data.created * 1000);
+
+    $('<p />').text('   Posted on: ' + postDate.getMonth() + '/' + postDate.getDay() +
+    '/' + postDate.getFullYear()).appendTo($container);
+  });
+    });
+
+});
